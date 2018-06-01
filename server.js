c77bffcefe6cf4887b34386b9e3cc6f6acca14ee
Jmo5896 <jmo5896@moore-clan.net>
2018-06-01 11:16:09 -0600

    notes

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 var port = process.env.PORT || 6969;
+var tableCount = 0;
+
+// data storage======================================
 var tables = [];
 var waitingList = [];
-var tableCount = 0;
+
 app.post("/reserve", (req, res) => {
     var newRes = req.body;
 
@@ -20,6 +23,7 @@ app.post("/reserve", (req, res) => {
     }
 })
 
+//routes==========================================
 app.get("/api/tables", (req, res) => {
     res.json(tables);
 })
@@ -35,6 +39,8 @@ app.get("/tables", (req, res) => {
 app.get("/reserve", (req, res) => {
     res.sendFile(path.join(__dirname, "reserve.html"));
 })
+
+//listener======================================
 app.listen(port, () => {
     console.log("listening at PORT: " + port);
 })
