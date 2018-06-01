55538ea89d48d168653c2a7fd2aa64e45f49e346
Jmo5896 <jmo5896@moore-clan.net>
2018-06-01 10:37:56 -0600

    pre-pull

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var port = 6969;
-
+var PORT = 6969;
 app.listen(port, () => {
     console.log("listening at PORT: " + port);
 })
