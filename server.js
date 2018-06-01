931b779bbf5d97f7ab2a03f8b80517db711ae083
Tyler Zonies <tylerzonies@gmail.com>
2018-06-01 11:07:16 -0600

    boy better have my money

--- server.js
+++ server.js
@@ -24,9 +24,17 @@ app.get("/api/tables", (req, res) => {
     res.json(tables);
 })
 app.get("/api/waitlist", (req, res) => {
-    res.json(waitlist);
+    res.json(waitingList);
 })
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
+})
+app.get("/tables", (req, res) => {
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
+app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
 app.listen(port, () => {
     console.log("listening at PORT: " + port);
 })
