ce1866934b798f23ede5769bbcbd112b9afe61af
Tyler Zonies <tylerzonies@gmail.com>
2018-06-01 10:36:11 -0600

    faZe three boiiii

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var port = 6969;
+var port = process.env.PORT || 6969;
+var tables = [];
+var waitingList = [];
+var tableCount = 0;
+app.post("/?", (req, res) => {
+    var newRes = req.body;
+
+    if(tableCount < 5){
+        tables.push(newRes);
+    }else{
+        waitingList.push(newRes);
+    }
+})
 
 app.listen(port, () => {
     console.log("listening at PORT: " + port);
